da6a0f01ec568a461e54f035430fa4a284cc2333
Jordan Tangy <jordantangy@gmail.com>
2019-12-01 21:55:16 +0200

    fixing tests

--- src/Ex1/MonomTest.java
+++ src/Ex1/MonomTest.java
@@ -1,100 +1,50 @@
 package Ex1;
-import java.util.ArrayList;
-/**
- * This class represents a simple (naive) tester for the Monom class, 
- * Note: <br>
- * (i) The class is NOT a JUNIT - (i.e., educational reasons) - should be changed to a proper JUnit in Ex1. <br>
- * (ii) This tester should be extend in order to test ALL the methods and functionality of the Monom class.  <br>
- * (iii) Expected output:  <br>
- * *****  Test1:  *****  <br>
-0) 2.0    	isZero: false	 f(0) = 2.0  <br>
-1) -1.0x    	isZero: false	 f(1) = -1.0  <br>
-2) -3.2x^2    	isZero: false	 f(2) = -12.8  <br>
-3) 0    	isZero: true	 f(3) = 0.0  <br>
- *****  Test2:  *****  <br>
-0) 0    	isZero: true  	eq: true  <br>
-1) -1.0    	isZero: false  	eq: true  <br>
-2) -1.3x    	isZero: false  	eq: true  <br>
-3) -2.2x^2    	isZero: false  	eq: true  <br>
- */
-public class MonomTest {
-	public static void main(String[] args) {
-		test1();
-		test2();
-		test3();
-		System.out.println("********************");
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MonomTest {
+
+	@Test
+	void constructorsAndToString (){
+		assertEquals(new Monom("1").toString(), "1.0");
+		assertEquals(new Monom("-x").toString(), "-1.0x");
+		assertEquals(new Monom("2x^5").toString(), "2.0x^5");
+		assertEquals(new Monom("-x^3").toString(), "-1.0x^3");
+		assertEquals(new Monom("x^0").toString(), "1.0");
+		
+		
+		
+
+		
+		
 	}
-	private static void test1() {
-		System.out.println("*****  Test1:  *****");
-		String[] monoms = {"3x^2","0.5","2^2","x","-2","-x","2x^0","0x^2","","(2x^5)","2sdx^3","2x^3a","1x","1x^1","3x^-5","-5x^3","3^3x^3","3+3x^2","2^3+3","1234x^1234","x^x","3x^","2-2x^2"};
-		for(int i=0;i<monoms.length;i++) {
-			try {
-				Monom m = new Monom(monoms[i]);
-				String s = m.toString();
-				m = new Monom(s);
-				double fx = m.f(i);
-				System.out.println(i+") "+m +"    \tisZero: "+m.isZero()+"\t f("+i+") = "+fx+"     " + monoms[i]);
-			}
-			catch (Exception e) {
-				System.out.println(i+") " + e.getLocalizedMessage()+", String: " + monoms[i]);
-			}
-		}
-	}
-
-
-	private static void test2() {
-		System.out.println("*****  Test2:  *****");
-		ArrayList<Monom> monoms = new ArrayList<Monom>();
-		try{
-			monoms.add(new Monom(0,5));
-			monoms.add(new Monom(-1,0));
-			monoms.add(new Monom(-1.3,1));
-			monoms.add(new Monom(-2.2,2));
-			monoms.add(new Monom(-560.3,14));
-			monoms.add(new Monom(0,0));
-			monoms.add(new Monom(-1,2));
-			monoms.add(new Monom(-2,0));
-			monoms.add(new Monom(-0,0));
-			monoms.add(new Monom(0,2));
-			monoms.add(new Monom(-3,450));
-			monoms.add(new Monom(0,0));
-			monoms.add(new Monom(1,-1));
-			monoms.add(new Monom(1234,1234));
-		}catch (Exception e) {
-			System.out.println(e.getLocalizedMessage());
-		}
-
-		for(int i=0;i<monoms.size();i++) {
-			Monom m = new Monom(monoms.get(i));
-			String s = m.toString();
-			Monom m1 = new Monom(s);
-			boolean e = m.equals(m1);
-			System.out.println(i+") "+m +"    \tisZero: "+m.isZero()+"  \teq: "+e);
-		}
-	}
-
-	private static void test3() {	//test for add, multipy and substract.
-		System.out.println("*****  Test3:  *****");
-		String[] monoms = {"2x", "-x","-3.2x^2","0","1x","0x","2.0x^4","0.5"};
-		for(int i=0;i<monoms.length-1;i++) {
-			Monom one = new Monom(monoms[i]);
-			Monom two = new Monom(monoms[i+1]);
-			one.add(two);
-			Monom add = new Monom(one);
-			one = new Monom(monoms[i]);
-			one.multipy(two);
-			Monom multiply = new Monom(one);
-			one = new Monom(monoms[i]);
-			one.substract(two);
-			Monom substract = new Monom(one);
-			one = new Monom(monoms[i]);
-			System.out.println(i+") monom1: " + one + " monom2: " + two + "   add: " + add + "   multiply: " + multiply + " substract: " + substract);
-
-
-
-		}
-
-
+	 
+	void addAndMultipy (){
+		Monom m1 = new Monom("x^5");
+		Monom m2 = new Monom("8x^2");
+		Monom one = new Monom("1");
+		Monom minusone = new Monom("-1");
+		Monom Zero = new Monom("0");
+		m1.add(m2);
+		assertEquals(m1.toString(), "1.0x^5");
+		m1.multipy(m2);
+		assertEquals(m1.toString(), "8.0x^7");
+		m1 = new Monom("x^5");
+		m1.add(one);
+		assertEquals(m1.toString(), "1.0x^5");
+		m1.multipy(one);
+		assertEquals(m1.toString(), "1.0x^5");
+		m1.add(minusone);
+		assertEquals(m1.toString(), "1.0x^5");
+		m1.multipy(minusone);
+		assertEquals(m1.toString(), "-1.0x^5");
+		m1 = new Monom("x^5");
+		m1.add(Zero);
+		assertEquals(m1.toString(), "1.0x^5");
+		m1.multipy(Zero);
+		assertEquals(m1.toString(), "0.0");
 	}
 
 }
--- src/Ex1/PolynomTest.java
+++ src/Ex1/PolynomTest.java
@@ -1,139 +1,49 @@
 package Ex1;
 
-public class PolynomTest {
-	public static void main(String[] args) {
-		test1();
-		test2();
-		test3();
-		test4();
-		test5();
-		test6();
-		System.out.println("**********************");
+import static org.junit.jupiter.api.Assertions.*;
 
-	}
-	public static void test1() {
-		System.out.println("******  Test1:  ******");
-		Polynom p1 = new Polynom();
-		String[] monoms = {"1","x","x^2", "0.5x^2"};
-		for (int i = 0 ; i  <monoms.length ; i++) {
-			Monom m = new Monom(monoms[i]);
-			p1.add(m);
-		}
-		double aa = p1.area(0, 1, 0.0001);
-		p1.substract(p1);
-		System.out.println(p1.toString());
-		System.out.println(aa);
+import org.junit.jupiter.api.Test;
 
+class PolynomTest {
+
+	@Test
+	void ConstructorAndtoString() {
+		
+		
+		assertEquals(new Polynom("x^2+x"), "x^2+x");
+		assertEquals(new Polynom("2x^2+5x"), "2x^2+5x");
+		assertEquals(new Polynom("-3x^3+7x"), "-3.0x^2+7x");
+		assertEquals(new Polynom("x^2+x"), "x^2+x");
+		assertEquals(new Polynom("-x^4+5x^4"), "-1.0x^4+5x^4");
+		
+		
 	}
-	public static void test2() {
-		System.out.println("******  Test2:  ******");
-		Polynom p1 = new Polynom(), p2 =  new Polynom();
-		String[] monoms1 = {"2", "-x","-3.2x^2","4","-1.5x^2"};
-		String[] monoms2 = {"5", "1.7x","3.2x^2","-3","-1.5x^2"};
-		for(int i=0;i<monoms1.length;i++) {
-			Monom m = new Monom(monoms1[i]);
-			p1.add(m);
-		}
-		for(int i=0;i<monoms2.length;i++) {
-			Monom m = new Monom(monoms2[i]);
-			p2.add(m);
-		}
-		System.out.println("p1: "+p1);
-		System.out.println("p2: "+p2);
-		p1.add(p2);
-		System.out.println("p1+p2: "+p1);
-		p1.multiply(p2);
-		System.out.println("(p1+p2)*p2: "+p1);
-		String s1 = p1.toString();
-		//Polynom_able pp1 = Polynom.parse(s1);
-		//System.out.println("from string: "+pp1);
+	
+	void AddPolynoms() {
+		
+		Polynom p1 = new Polynom("3x^5+2x^4-x");
+		Polynom p2 = new Polynom("10x^5+4x^4-2x");
+		Polynom p3 = new Polynom("3x^6+2x^4+x^2");
+		Polynom p4 = new Polynom("4x^3+2x^2-x");
+		Polynom p5 = new Polynom("-5x^5+2x^2+3x");
+		Polynom p6 = new Polynom("5x^5-2x^2-3x");
+		Polynom p7 = new Polynom("5x^3+0x^3+1");
+		Polynom Zero = new Polynom("0");
+		Polynom p8 = new Polynom("x^3+0x^2");
+		
+		
+		p2.add(p4);
+		p3.add(p3);
+		p5.add(p6);
+		p7.add(Zero);
+		p8.add(Zero);
+		assertEquals(p2.toString(),"10.0x^5+4.0x^4+4.0x^3+2.0x^2-3.0x");
+		assertEquals(p5.toString(), "0");
+		assertEquals(p3.toString(), "6.0x^6+4.0x^4+2.0x^2");
+		assertEquals(p7.toString(), "5.0x^3+1");
+		assertEquals(p8.toString(), "1.0x^3" );
+		
+		
 	}
 
-
-	public static void test3() {//test for area and root.
-		System.out.println("******  Test3:  ******");
-		String[][] polynoms = {{"3x^2","-6x^3","9x","-2"},
-				{"x","5x","0","-5"},
-				{"4x^6", "-5x^5", "1"}};
-		for (int i = 0; i < polynoms.length; i++) {
-			Polynom p1 = new Polynom();
-			for (int j = 0; j < polynoms[i].length; j++) {
-				Monom temp = new Monom(polynoms[i][j]);
-				p1.add(temp);
-			}
-			System.out.println(i + ") " + p1 + " area: " + p1.area(-1, 0, 0.0001) + " root: " + p1.root(0, 1, 0.0001));
-		}
-	}
-
-
-	public static void test4() {//test for add and substract.
-		System.out.println("******  Test4:  ******");
-		String[][] polynoms = {{"3x^2","-6x^3","9x","-2"},
-				{"x","5x","0","-5"}};
-		Polynom p1 = new Polynom();
-		Polynom p2 = new Polynom();
-		for (int i = 0; i < polynoms[0].length; i++) {
-			Monom temp = new Monom(polynoms[0][i]);
-			p1.add(temp);
-		}		
-		for (int i = 0; i < polynoms[1].length; i++) {
-			Monom temp = new Monom(polynoms[1][i]);
-			p2.add(temp);
-		}
-		Polynom add = new Polynom();
-		Polynom substract = new Polynom();
-		add = (Polynom) p1.copy();
-		substract = (Polynom)p1.copy();
-		add.add(p2);
-		substract.substract(p2);
-		System.out.println("p1: " + p1);
-		System.out.println("p2: " + p2);
-		System.out.println("add: " + add);
-		System.out.println("substract: " + substract);
-	}
-
-
-	public static void test5() {//test for multiply.
-		System.out.println("******  Test5:  ******");
-		String[][] polynoms = {{"3x^2","-6x^3","9x","-2"},
-				{"1x"}};
-		Polynom p1 = new Polynom();
-		Polynom p2 = new Polynom();
-		for (int i = 0; i < polynoms[0].length; i++) {
-			Monom temp = new Monom(polynoms[0][i]);
-			p1.add(temp);
-		}		
-		for (int i = 0; i < polynoms[1].length; i++) {
-			Monom temp = new Monom(polynoms[1][i]);
-			p2.add(temp);
-		}
-		Polynom multiply = new Polynom();
-		multiply = (Polynom) p1.copy();
-		multiply.multiply(p2);
-
-		System.out.println("p1: " + p1);
-		System.out.println("p2: " + p2);
-		System.out.println("multiply: " + multiply);
-
-	}
-
-
-	public static void test6() {//test for equals.
-		System.out.println("******  Test6:  ******");
-		String[][] polynoms = {{"3x^2","-6x^3","9x","-2"},
-				{"x","5x","0","-5"}};
-		Polynom p1 = new Polynom();
-		Polynom p2 = new Polynom();
-		for (int i = 0; i < polynoms[0].length; i++) {
-			Monom temp = new Monom(polynoms[0][i]);
-			p1.add(temp);
-		}		
-		for (int i = 0; i < polynoms[1].length; i++) {
-			Monom temp = new Monom(polynoms[1][i]);
-			p2.add(temp);
-		}
-		System.out.println("p1: " + p1);
-		System.out.println("p2: " + p2);
-		System.out.println(p1.equals(p2));
-	}
 }
