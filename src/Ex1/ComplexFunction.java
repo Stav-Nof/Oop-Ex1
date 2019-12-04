7b971a18ec74fc6130003dd3ebff8037c83d30cc
Yarin Shapira <yariniii19@gmail.com>
2019-12-04 13:25:48 +0200

    adding initFromString to ComplexFunction

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -32,176 +32,215 @@ public class ComplexFunction implements complex_function{
 
 	@Override
 	public function initFromString(String s) {
-		// TODO Auto-generated method stub
+		if(!(s.charAt(0) == 'P') || !(s.charAt(0) == 'p')|| !(s.charAt(0) == 'T') || !(s.charAt(0) == 't')|| !(s.charAt(0) == 'M') || !(s.charAt(0) == 'm')|| !(s.charAt(0) == 'D') || !(s.charAt(0) == 'd')|| !(s.charAt(0) == 'M') || !(s.charAt(0) == 'm')|| !(s.charAt(0) == 'C') || !(s.charAt(0) == 'c')) {
+			Polynom temp = new Polynom(s);
+			return temp;
+		}
+		ComplexFunction f1;
+		ComplexFunction f2;
+		String operation = "";
+		int counter = 0;
+		int i = 0;
+		for (; i < s.length(); i++) {
+			if (s.charAt(i) != '(') {
+				operation = operation + s.charAt(i);
+			}
+			else {
+				counter++;
+				i++;
+				break;
+			}
+		}
+		String func1 = "";
+		for (; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
+				counter++;
+			}
+			if (s.charAt(i) == ')') {
+				counter--;
+			}
+			if(s.charAt(i) == ',' && counter == 1) {
+				i++;
+				break;
+			}
+			func1 = func1 + s.charAt(i);
+		}
+		String func2 = "";
+		for (; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
+				counter++;
+			}
+			if (s.charAt(i) == ')') {
+				counter--;
+			}
+			if(s.charAt(i) == ')' && counter == 0) {
+				i++;
+				break;
+			}
+			func2 = func2 + s.charAt(i);
+		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		return null;
+		ComplexFunction ans = new ComplexFunction(operation, initFromString(func1),initFromString(func2));
+		return ans;
 	}
 
 	@Override
 	public function copy() {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
 		return null;
 	}
 
 	@Override
 	public void plus(function f1) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	}
 
 	@Override
 	public void mul(function f1) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	}
 
 	@Override
 	public void div(function f1) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	}
 
 	@Override
 	public void max(function f1) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	}
 
 	@Override
 	public void min(function f1) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	}
 
 	@Override
 	public void comp(function f1) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	}
 
 	@Override
 	public function left() {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
 		return null;
 	}
 
 	@Override
 	public function right() {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
 		return null;
 	}
 
 	@Override
 	public Operation getOp() {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
 		return null;
 	}
 
