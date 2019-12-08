03d9ba4afcbab81b4a45ec778c8de5a460ef6779
Yarin Shapira <yariniii19@gmail.com>
2019-12-08 20:21:45 +0200

    small fix

--- src/Ex1/Functions_GUI.java
+++ src/Ex1/Functions_GUI.java
@@ -11,33 +11,33 @@ import Ex1.StdDraw;;
 
 public class Functions_GUI implements functions {
 	private LinkedList<function> Functions;
-	
-	
+
+
 	@Override
 	public int size() {
 		return Functions.size();
 	}
 
-	
+
 	@Override
 	public boolean isEmpty() {
 		return Functions.isEmpty();
 	}
 
-	
+
 	@Override
 	public boolean contains(Object o) {
 		return Functions.contains(o);
 	}
 
-	
+
 	@Override
 	public Iterator<function> iterator() {
 		Iterator<function> iterator = this.Functions.iterator();
 		return iterator;
 	}
 
-	
+
 	@Override
 	public Object[] toArray() {
 		function[] ans = new function[this.size()];
@@ -45,7 +45,7 @@ public class Functions_GUI implements functions {
 		return ans;
 	}
 
-	
+
 	@Override
 	public <T> T[] toArray(T[] a) {
 		if(a.length < this.size()) {
@@ -59,11 +59,10 @@ public class Functions_GUI implements functions {
 		}
 		return a;
 	}
-	
+
 
 	@Override
 	public boolean add(function e) {
-		// TODO Auto-generated method stub
 		
 		Iterator<function> iterator = this.iterator();
 		boolean flag = false;
@@ -78,62 +77,49 @@ public class Functions_GUI implements functions {
 				break;
 			}
 		}
-		
 		return flag;
-		
-		
-
 	}
 
+	
 	@Override
 	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		
 		Iterator<function> iterator = this.iterator();
 		boolean b = false;
 		while(iterator.hasNext()) {
-			function it = iterator.next();
-			if (it.equals(o)) {
-				this.Functions.remove(it);
-				b = true;
+			function thiso = iterator.next();
+			if (thiso.equals(o)) {
+				this.Functions.remove(thiso);
+				return true;
 			}
-				if (b == true) {
-					break;
-				}
-				
-			}
-		
-		return b;
-		
+		}
+		return false;
 	}
-
+	
+	
 	@Override
 	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		
 		if(this.size() < c.size()) {
 			return false;
 		}
-		
-		Iterator<?> iterator = this.iterator();
-		
-		Iterator<?> iterator1 = c.iterator();
-		
-		boolean flag = false;
-		for (int i = 0; i < this.size(); i++) {
-			Object o = iterator.next();
-			for (int j = 0; j < c.size(); j++) {
-				Object o1 = iterator1.next();
-				if(o1.equals(o)) {
+		Iterator<?> thisIterator = this.iterator();
+		Iterator<?> cIterator = c.iterator();
+		while(cIterator.hasNext()) {
+			boolean flag = false;
+			Object co = cIterator.next();
+			while(thisIterator.hasNext()) {
+				Object thiso = thisIterator.next();
+				if(co.equals(thiso)) {
 					flag = true;
-					i++;
 					break;
 				}
-				
+			}
+			if (!flag) {
+				return false;
 			}
 		}
-		return flag;
+		return true;
 	}
+
 
 	@Override
 	public boolean addAll(Collection<? extends function> c) {
@@ -156,32 +142,32 @@ public class Functions_GUI implements functions {
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void initFromFile(String file) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void saveToFile(String file) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void drawFunctions(String json_file) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 
 }
