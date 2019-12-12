21b84db2118a0d0e68d4e284523a695aa22205f4
Yarin Shapira <yariniii19@gmail.com>
2019-12-13 00:42:17 +0200

    last commit for ComplexFunction and ComplexFunctionTest

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -13,18 +13,18 @@ public class ComplexFunction implements complex_function {
 	function right;
 	Operation op;
 
-/*
- * Constructs an empty ComplexFunction
- */
+	/*
+	 * Constructs an empty ComplexFunction
+	 */
 	public ComplexFunction() {
 		this.left = null;
 		this.right = null;
 		this.op = Operation.None;
 	}
 
-/*
- * Constructs a ComplexFunction by receiving a String(which is the operation), a function on its left and right side.
- */
+	/*
+	 * Constructs a ComplexFunction by receiving a String(which is the operation), a function on its left and right side.
+	 */
 	public ComplexFunction(String s, function left, function right) {
 		s = s.toLowerCase();
 		this.op = StringToOperation(s);
@@ -62,12 +62,12 @@ public class ComplexFunction implements complex_function {
 		initFromString(f.toString());
 	}
 
-/*
- * Constructs a ComplexFunction from a String only. This String is containing an operation (or several) and a function on 
- * each side (or severals). This Constructor is separating between the elements and aims to differentiates between the characters in order to 
- * get the informations we want to create a real ComplexFunction.
- * The constructor is using recursion.
- */
+	/*
+	 * Constructs a ComplexFunction from a String only. This String is containing an operation (or several) and a function on 
+	 * each side (or severals). This Constructor is separating between the elements and aims to differentiates between the characters in order to 
+	 * get the informations we want to create a real ComplexFunction.
+	 * The constructor is using recursion.
+	 */
 	public ComplexFunction(String s) {
 		s = s.toLowerCase();
 		if(s.isEmpty()) {
@@ -141,12 +141,12 @@ public class ComplexFunction implements complex_function {
 		}
 	}
 
-/*
- * This function is used by the second, the fourth and the fifth Constructor (above). 
- * The String is analyzed and depending on the String contains, the function is returning an Operation from 
- * the enum class. This function is very useful in order to get the operation when a ComplexFunction is created by a String or 
- * at least its Operation.
- */
+	/*
+	 * This function is used by the second, the fourth and the fifth Constructor (above). 
+	 * The String is analyzed and depending on the String contains, the function is returning an Operation from 
+	 * the enum class. This function is very useful in order to get the operation when a ComplexFunction is created by a String or 
+	 * at least its Operation.
+	 */
 	public static Operation StringToOperation(String s) {
 		s = s.toLowerCase();
 		if (s.equals("plus") || s.equals("add")) {
@@ -170,12 +170,12 @@ public class ComplexFunction implements complex_function {
 		return Operation.None;
 	}
 
-/*
- * Implementing the f function from complex_function interface.
- * Computes the value of f(x) for a given number x.
- * In more detailed, the function is computing the value for each side of the function and
- * then performs the operation between each side (Operation from enum class).
- */
+	/*
+	 * Implementing the f function from complex_function interface.
+	 * Computes the value of f(x) for a given number x.
+	 * In more detailed, the function is computing the value for each side of the function and
+	 * then performs the operation between each side (Operation from enum class).
+	 */
 	@Override
 	public double f(double x) {
 		double l = 0;
@@ -207,11 +207,11 @@ public class ComplexFunction implements complex_function {
 		return 0;
 	}
 
-/*
- * Implementing the initFromString function from complex_function interface.
- * Initialize a ComplexFunction of type "function" from a String by simply sending the String to the proper
- * ComplexFunction constructor.
- */
+	/*
+	 * Implementing the initFromString function from complex_function interface.
+	 * Initialize a ComplexFunction of type "function" from a String by simply sending the String to the proper
+	 * ComplexFunction constructor.
+	 */
 	@Override
 	public function initFromString(String s) {
 		if((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == 'x' || s.charAt(0) == '-' || s.charAt(0) == '+') {
@@ -221,10 +221,10 @@ public class ComplexFunction implements complex_function {
 		return ans;
 	}
 
-/*
- * Function that turns a function into a String.
- * Returns a String.
- */
+	/*
+	 * Function that turns a function into a String.
+	 * Returns a String.
+	 */
 	public String toString() {
 		String ans = "";
 		if (this.left != null && this.right != null) {
@@ -236,10 +236,10 @@ public class ComplexFunction implements complex_function {
 		return ans;
 	}
 
-/*
- * Implementing the copy function from complex_function interface.
- * Performs a deep copy of a given function.
- */
+	/*
+	 * Implementing the copy function from complex_function interface.
+	 * Performs a deep copy of a given function.
+	 */
 	@Override
 	public function copy() {
 		String temp = this.toString();
@@ -247,132 +247,186 @@ public class ComplexFunction implements complex_function {
 		return ans;
 	}
 
-/*
- * Implementing the plus function from complex_function interface.
- * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
- * Sets the operation "plus" between this function and f1.
- */
+	/*
+	 * Implementing the plus function from complex_function interface.
+	 * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
+	 * Sets the operation "plus" between this function and f1.
+	 */
 	@Override
 	public void plus(function f1) {
+		if (this.left == null && this.right == null) {
+			this.left = f1.copy();
+			return;
+		}
+		if(this.right == null) {
+			this.op = Operation.Plus;
+			this.right = f1.copy();
+			return;
+		}
 		function temp = this.copy();
 		this.left = temp;
 		this.op = Operation.Plus;
-		this.right  = f1;
+		this.right = f1;
 	}
 
-/*
- * Implementing the mul function from complex_function interface.
- * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
- * Sets the operation "multiply" between this function and f1.
- */	
+	/*
+	 * Implementing the mul function from complex_function interface.
+	 * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
+	 * Sets the operation "multiply" between this function and f1.
+	 */	
 	@Override
 	public void mul(function f1) {
+		if (this.left == null && this.right == null) {
+			this.left = f1.copy();
+			return;
+		}
+		if(this.right == null) {
+			this.op = Operation.Times;
+			this.right = f1.copy();
+			return;
+		}
 		function temp = this.copy();
 		this.left = temp;
 		this.op = Operation.Times;
-		this.right  = f1;
+		this.right = f1;
 	}
 
-/*
- * Implementing the div function from complex_function interface.
- * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
- * Sets the operation "divide" between this function and f1.
- */
+	/*
+	 * Implementing the div function from complex_function interface.
+	 * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
+	 * Sets the operation "divide" between this function and f1.
+	 */
 	@Override
 	public void div(function f1) {
+		if (this.left == null && this.right == null) {
+			this.left = f1.copy();
+			return;
+		}
+		if(this.right == null) {
+			this.op = Operation.Divid;
+			this.right = f1.copy();
+			return;
+		}
 		function temp = this.copy();
 		this.left = temp;
 		this.op = Operation.Divid;
 		this.right = f1;
 	}
 
-/*
- * Implementing the max function from complex_function interface.
- * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
- * Sets the operation "maximum" between this function and f1.
- */
+	/*
+	 * Implementing the max function from complex_function interface.
+	 * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
+	 * Sets the operation "maximum" between this function and f1.
+	 */
 	@Override
 	public void max(function f1) {
+		if (this.left == null && this.right == null) {
+			this.left = f1.copy();
+			return;
+		}
+		if(this.right == null) {
+			this.op = Operation.Max;
+			this.right = f1.copy();
+			return;
+		}
 		function temp = this.copy();
 		this.left = temp;
 		this.op = Operation.Max;
 		this.right = f1;
 	}
 
-/*
- * Implementing the min function from complex_function interface.
- * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
- * Sets the operation "minimum" between this function and f1.
- */
+	/*
+	 * Implementing the min function from complex_function interface.
+	 * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
+	 * Sets the operation "minimum" between this function and f1.
+	 */
 	@Override
 	public void min(function f1) {
+		if (this.left == null && this.right == null) {
+			this.left = f1.copy();
+			return;
+		}
+		if(this.right == null) {
+			this.op = Operation.Min;
+			this.right = f1.copy();
+			return;
+		}
 		function temp = this.copy();
 		this.left = temp;
 		this.op = Operation.Min;
-		this.right  = f1;
+		this.right = f1;
 	}
 
-/*
- * Implementing the comp function from complex_function interface.
- * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
- * Sets the operation "compare" between this function and f1.
- */
+	/*
+	 * Implementing the comp function from complex_function interface.
+	 * Copies this function and put it on left side of the ComplexFunction and f1 on the right side of it. 
+	 * Sets the operation "compare" between this function and f1.
+	 */
 	@Override
 	public void comp(function f1) {
+		if (this.left == null && this.right == null) {
+			this.left = f1.copy();
+			return;
+		}
+		if(this.right == null) {
+			this.op = Operation.Comp;
+			this.right = f1.copy();
+			return;
+		}
 		function temp = this.copy();
 		this.left = temp;
 		this.op = Operation.Comp;
-		this.right  = f1;
+		this.right = f1;
 	}
 
-/*
- * Implementing the left function from complex_function interface.
- * Returns the left side of this function.
- */
+	/*
+	 * Implementing the left function from complex_function interface.
+	 * Returns the left side of this function.
+	 */
 	@Override
 	public function left() {
 		return this.left;
 	}
 
-/*
- * Implementing the right function from complex_function interface.
- * Returns the right side of this function.
- */
+	/*
+	 * Implementing the right function from complex_function interface.
+	 * Returns the right side of this function.
+	 */
 	@Override
 	public function right() {
 		return this.right;
 	}
 
-/*
- * Implementing the getOp function from complex_function interface.
- * Returns the Operation of this function.
- */
+	/*
+	 * Implementing the getOp function from complex_function interface.
+	 * Returns the Operation of this function.
+	 */
 	@Override
 	public Operation getOp() {
 		return this.op;
 	}
 
-/*
- * Compares between the Strings of this ComplexFunction and another given ComplexFunction.
- * Returns true or false.
- */
+	/*
+	 * Compares between the Strings of this ComplexFunction and another given ComplexFunction.
+	 * Returns true or false.
+	 */
 	public boolean equals(ComplexFunction cf) {
 		String s1 = this.toString();
 		String s2 = cf.toString();
 		return s1.equals(s2);
 	}
 
-/*
- * This function is checking if this ComplexFunction is equal to another given ComplexFunction.
- * The user gives a range (start,end), and a number of steps.
- * The equality is verified only if the calculations of f(x) for this function and the given function are equal for 
- * all the values between the starting point to the end point (both of them included) in a certain number of steps. 
- * If the calculation for both of them is equal, the function returns true. If not the function returns false.
- */
+	/*
+	 * This function is checking if this ComplexFunction is equal to another given ComplexFunction.
+	 * The user gives a range (start,end), and a number of steps.
+	 * The equality is verified only if the calculations of f(x) for this function and the given function are equal for 
+	 * all the values between the starting point to the end point (both of them included) in a certain number of steps. 
+	 * If the calculation for both of them is equal, the function returns true. If not the function returns false.
+	 */
 	public boolean equalsIn(ComplexFunction cf, double start, double end,int steps) {
 		double plus = Math.abs(end-start) / steps;
 		for (double i = start; i <= end; i = i + plus) {
-			if (!(this.f(i) == cf.f(i))) {
+			if (this.f(i) != cf.f(i)) {
 				return false;
 			}
 		}
--- src/Ex1Testing/ComplexFunctionTest.java
+++ src/Ex1Testing/ComplexFunctionTest.java
@@ -12,212 +12,229 @@ import Ex1.function;
 
 class ComplexFunctionTest {
 
-	
-	@Test
-	void firstConstructor() {
-		String s1 = "3.1+2.4x^2-x^4";
-		String s2 = "5+2x-3.3x+0.1x^5";
-		String[] s3 = {"x+3","x-2", "x-4"};
-		Polynom p1 = new Polynom(s1);
-		Polynom p2 = new Polynom(s2);
-		Monom m = new Monom("2x^2");
-		Monom m1 = new Monom("-x");
-		Polynom p3 = new Polynom("3x^2-5x+2");
-		Polynom p4 = new Polynom("2x^2+5x");
 
+	@Test
+	void constructors() {
+
+
+
+	}
+
+
+	@Test
+	void constructorfirstTest() {
+		ComplexFunction t1 = new ComplexFunction();
+		assertEquals(t1.toString(), "");
+
+		Polynom p1 = new Polynom("3.1+2.4x^2-x^4");
+		Polynom p2 = new Polynom("5+2x-3.3x+0.1x^5");
+		Monom m1 = new Monom("-x");
 
 		ComplexFunction cf = new ComplexFunction("div",p1,p2);
 		ComplexFunction cf1 = new ComplexFunction("div",p1,p2);
-		ComplexFunction cf2 = new ComplexFunction("plus",new Polynom(s3[1]), p2 );
-		ComplexFunction cf3 = new ComplexFunction("div(mul(2x^2,3x^2-5x+2),-x)");
-		ComplexFunction cf4 = new ComplexFunction("mul(2x^2,3x^2-5x+2)");
-		ComplexFunction cf5 = new ComplexFunction("div", cf4, m1 );
-		ComplexFunction cf6 = new ComplexFunction("div(mul(2x,3x^2-5x+2),-x");
-		ComplexFunction cf7 = new ComplexFunction("div(3.1+2.4x^2-x^4,5+2x-3.3x+0.1x^5)");
+		ComplexFunction cf2 = new ComplexFunction("div(mul(2x^2,3x^2-5x+2),-x)");
+		ComplexFunction cf3 = new ComplexFunction("mul(2x^2,3x^2-5x+2)");
+		ComplexFunction cf4 = new ComplexFunction("div", cf3, m1 );
+		ComplexFunction cf5 = new ComplexFunction("div(mul(2x,3x^2-5x+2),-x");
+		ComplexFunction cf6 = new ComplexFunction("div(3.1+2.4x^2-x^4,5+2x-3.3x+0.1x^5)");
 
-		
 		assertTrue(cf.equals(cf1));
-		assertTrue(cf5.equals(cf3));
-		assertNotEquals(cf3.toString(),cf6.toString());
-		assertEquals(cf7.toString(), cf.toString());
+		assertTrue(cf4.equals(cf2));
+		assertNotEquals(cf2.toString(),cf5.toString());
+		assertEquals(cf6.toString(), cf.toString());
 	}
 
-	
+
 	@Test
-	void secondConstructor() {
-		String s1 = "3.1+2.4x^2-x^4";
-		String s2 = "5+2x-3.3x+0.1x^5";
-		String[] s3 = {"x+3","2x^2-5x+1", "x-4"};
-		Polynom p1 = new Polynom(s1);
-		Polynom p2 = new Polynom(s2);
-		Polynom p5 = new Polynom("-3x^2+2x^3");
+	void constructorSecondTest() {
+		String s1 = "2x^2-5x+1";
 		Monom m = new Monom("4x^2");
 		Monom m1 = new Monom("-2x");
 		Monom m2 = new Monom("4x^5");
 		Monom m3 = new Monom("-2x^4");
-		Polynom p3 = new Polynom("3x^2-5x+2");
-		Polynom p4 = new Polynom("2x^2+5x");
-
+		Polynom p1 = new Polynom("2x^2+5x");
 
 		ComplexFunction cf = new ComplexFunction(Operation.Divid,m,m1);
 		ComplexFunction cf1 = new ComplexFunction(Operation.Divid,m2,m3);
-		ComplexFunction cf2 = new ComplexFunction(Operation.Times,new Polynom(s3[1]), p4);
-		ComplexFunction cf3 = new ComplexFunction(Operation.Max, new Polynom("2x^2-5x+1"), p4);
-		ComplexFunction cf4 = new ComplexFunction("max", new Polynom(s3[1]), p4 );
-		ComplexFunction cf5 = new ComplexFunction("div", new Polynom("5+2x-3.3x+0.1x^5"), m1 );
-		ComplexFunction cf6 = new ComplexFunction(Operation.Divid , p2, p3);
-		ComplexFunction cf7 = new ComplexFunction("div", new Polynom("5+2x-3.3x+0.1x^5"), new Monom("-2x") );
+		ComplexFunction cf2 = new ComplexFunction(Operation.Max, new Polynom("2x^2-5x+1"), p1);
+		ComplexFunction cf3 = new ComplexFunction("max", new Polynom(s1), p1 );
+		ComplexFunction cf4 = new ComplexFunction("div", new Polynom("5+2x-3.3x+0.1x^5"), m1 );
+		ComplexFunction cf5 = new ComplexFunction("div", new Polynom("5+2x-3.3x+0.1x^5"), new Monom("-2x") );
 
 		assertFalse(cf.equals(cf1));
-		assertTrue(cf3.equals(cf4));
-		assertEquals(cf3.toString(), cf4.toString());
-		assertTrue(cf5.equals(cf7));
+		assertTrue(cf2.equals(cf3));
+		assertEquals(cf2.toString(), cf3.toString());
+		assertTrue(cf4.equals(cf5));
 	}
 
-	
+
 	@Test
-	void thirdConstructor() {
-		String s1 = "3.1+2.4x^2-x^4";
-		String s2 = "5+2x-3.3x+0.1x^5";
-		String[] s3 = {"x+3","x-2", "x-4"};
-		Polynom p1 = new Polynom(s1);
-		Polynom p2 = new Polynom(s2);
+	void constructorThirdTest() {
 		Monom m = new Monom("4x^2");
 		Monom m1 = new Monom("-2x");
-		Monom m2 = new Monom("4x^5");
-		Monom m3 = new Monom("-2x^4");
-		Polynom p3 = new Polynom("3x^2-5x+2");
-		Polynom p4 = new Polynom("2x^2+5x");
-		String s4 = "mul(3x^2,5x)";
 		ComplexFunction cf = new ComplexFunction(Operation.Divid,m,m1);
-		ComplexFunction cf1 = new ComplexFunction(Operation.Plus,m2,m3);
-		ComplexFunction cf2 = new ComplexFunction(Operation.Times,new Polynom(s3[1]), p2);
-		ComplexFunction cf3 = new ComplexFunction(Operation.Max, new Polynom(s3[0]), p4);
-		ComplexFunction cf4 = new ComplexFunction("mul(plus(3x^2+9x^4+6x^5,10x-5x^2),-6x^6+7x^2)");
-		ComplexFunction cf5 = new ComplexFunction(Operation.Plus, new Polynom ("3x^2+9x^4+6x^5"), new Polynom("10x-5x^2"));
-		ComplexFunction cf7 = new ComplexFunction(Operation.Times , cf5 , new Polynom("-6x^6+7x^2"));
-		ComplexFunction cf6 = new ComplexFunction("div", cf4, m1 );
-
-		ComplexFunction f = (ComplexFunction) cf.initFromString(cf7.toString());
-		assertEquals(cf4.toString(), f.toString());
-		assertTrue(cf4.equals(f));
+		ComplexFunction cf1 = new ComplexFunction("mul(plus(3x^2+9x^4+6x^5,10x-5x^2),-6x^6+7x^2)");
+		ComplexFunction cf2 = new ComplexFunction(Operation.Plus, new Polynom ("3x^2+9x^4+6x^5"), new Polynom("10x-5x^2"));
+		ComplexFunction cf3 = new ComplexFunction(Operation.Times , cf2 , new Polynom("-6x^6+7x^2"));
+		ComplexFunction f = (ComplexFunction) cf.initFromString(cf3.toString());
+		assertEquals(cf1.toString(), f.toString());
+		assertTrue(cf1.equals(f));
 	}
-
-
 
 
 	@Test
 	void testF() {
-
-		double exp = -12;
-		double exp2 = -1/3;
-		double exp3 = 690;
-		ComplexFunction cf5 = new ComplexFunction(Operation.Times, new Polynom ("-5x^2+3x"), new Polynom("7x^2-x"));
-		ComplexFunction cf6 = new ComplexFunction(Operation.Divid, new Polynom ("-5x^2+3x"), new Polynom("7x^2-x"));
-		ComplexFunction cf7 = new ComplexFunction(Operation.Max, new Polynom ("-5x^2+3x"), new Polynom("7x^2-x"));
-		double actual = cf5.f(1);
-		double actual2 = cf6.f(1);
-		double actual3 = cf7.f(10);
-		assertEquals(exp,actual);
-
-
+		//Plus
+		ComplexFunction cf1 = new ComplexFunction(Operation.Plus, new Polynom ("-5x^2+3x"), new Polynom("7x^2-x"));
+		assertEquals(40,cf1.f(-5));
+		assertEquals(0,cf1.f(-1));
+		assertEquals(0,cf1.f(0));
+		assertEquals(4,cf1.f(1));
+		assertEquals(60,cf1.f(5));
+		//Times
+		ComplexFunction cf2 = new ComplexFunction(Operation.Times, new Polynom ("-5x^2+3x"), new Polynom("7x^2-x"));
+		assertEquals(-25200,cf2.f(-5));
+		assertEquals(-64,cf2.f(-1));
+		assertEquals(0,cf2.f(0));
+		assertEquals(-12,cf2.f(1));
+		assertEquals(-18700,cf2.f(5));
+		//Divid
+		ComplexFunction cf3 = new ComplexFunction(Operation.Divid, new Polynom ("-5x^2+3x"), new Polynom("7x^2-x"));
+		assertEquals(-0.7777777777777778,cf3.f(-5));
+		assertEquals(-1.0,cf3.f(-1));
+		//assertEquals(0,cf3.f(0));//undefined
+		assertEquals(-1.0/3.0,cf3.f(1));
+		assertEquals(-0.6470588235294118,cf3.f(5));
+		//Max
+		ComplexFunction cf4 = new ComplexFunction(Operation.Max, new Polynom ("7x^2-5"), new Polynom("x"));
+		assertEquals(170,cf4.f(-5));
+		assertEquals(2,cf4.f(-1));
+		assertEquals(0,cf4.f(0));
+		assertEquals(2.,cf4.f(1));
+		assertEquals(170,cf4.f(5));
+		//Min
+		ComplexFunction cf5 = new ComplexFunction(Operation.Min, new Polynom ("7x^2-5"), new Polynom("x"));
+		assertEquals(-5,cf5.f(-5));
+		assertEquals(-1,cf5.f(-1));
+		assertEquals(-5,cf5.f(0));
+		assertEquals(1.,cf5.f(1));
+		assertEquals(5,cf5.f(5));
+		//Comp
+		ComplexFunction cf6 = new ComplexFunction(Operation.Comp, new Polynom ("-5x^2+3x"), new Polynom("7x^2-x"));
+		assertEquals(-161460,cf6.f(-5));
+		assertEquals(-296,cf6.f(-1));
+		assertEquals(0,cf6.f(0));
+		assertEquals(-162,cf6.f(1));
+		assertEquals(-143990,cf6.f(5));
 	}
 
 
 	@Test
 	void testCopy() {
-
-		ComplexFunction cf3 = new ComplexFunction("div(mul(2x^2,3x^2-5x+2),-x)");
-		ComplexFunction cf4 = (ComplexFunction) cf3.copy();
-		assertEquals(cf3.toString(),cf4.toString());
-
-
+		ComplexFunction cf1 = new ComplexFunction("div(mul(2x^2,3x^2-5x+2),-x)");
+		ComplexFunction cf2 = (ComplexFunction) cf1.copy();
+		assertEquals(cf1.toString(),cf2.toString());
 	}
 
-	
+
+	void testPplus() {
+		String s = "3x^2-10x+2";
+		ComplexFunction cf1 = new ComplexFunction("plus(2x^2,3x^2-5x+2)");
+		function f1 = cf1.initFromString(s);
+		cf1.mul(f1);
+		ComplexFunction cf = new ComplexFunction(Operation.Times, new ComplexFunction("plus(2x^2,3x^2-5x+2)"), new Polynom("3x^2-10x+2"));
+		assertTrue(cf1.equals(cf));
+		assertEquals(cf1.toString(), cf.toString());
+	}
+
+
 	@Test
 	void testMul() {
-		String s2 ="3x^2-10x+2";
-		ComplexFunction cf3 = new ComplexFunction("plus(2x^2,3x^2-5x+2)");
-
-		function f1 = cf3.initFromString(s2);
-		cf3.mul(f1);
-
+		String s ="3x^2-10x+2";
+		ComplexFunction cf1 = new ComplexFunction("plus(2x^2,3x^2-5x+2)");
+		function f1 = cf1.initFromString(s);
+		cf1.mul(f1);
 		ComplexFunction cf = new ComplexFunction(Operation.Times, new ComplexFunction("plus(2x^2,3x^2-5x+2)"), new Polynom("3x^2-10x+2"));
-		assertTrue(cf3.equals(cf));
-		assertEquals(cf3.toString(), cf.toString());
+		assertTrue(cf1.equals(cf));
+		assertEquals(cf1.toString(), cf.toString());
 	}
 
-	
+
 	@Test
 	void testDiv() {
 		String s = "x^2+3x-5";
-		ComplexFunction cf3 = new ComplexFunction("plus(2x^2-2x,3x^2)");
-
-		function f1 = cf3.initFromString(s);
-		cf3.div(f1);
-
+		ComplexFunction cf1 = new ComplexFunction("plus(2x^2-2x,3x^2)");
+		function f1 = cf1.initFromString(s);
+		cf1.div(f1);
 		ComplexFunction cf = new ComplexFunction(Operation.Divid, new ComplexFunction("plus(2x^2-2x,3x^2"), new Polynom("x^2+3x-5"));
-		assertTrue(cf3.equals(cf));
-		assertEquals(cf3.toString(), cf.toString());
+		assertTrue(cf1.equals(cf));
+		assertEquals(cf1.toString(), cf.toString());
 	}
-	
+
 
 	@Test
 	void testMax() {
-		String s2 ="-x^2";
-		ComplexFunction cf3 = new ComplexFunction("div(2x^2,3x^2)");
-		function f1 = cf3.initFromString(s2);
-		cf3.max(f1);
+		String s ="-x^2";
+		ComplexFunction cf1 = new ComplexFunction("div(2x^2,3x^2)");
+		function f1 = cf1.initFromString(s);
+		cf1.max(f1);
 		ComplexFunction cf = new ComplexFunction(Operation.Max, new ComplexFunction("div(2x^2,3x^2)"), new Monom("-x^2"));
-		assertTrue(cf3.equals(cf));
-		assertEquals(cf3.toString(), cf.toString());
+		assertTrue(cf1.equals(cf));
+		assertEquals(cf1.toString(), cf.toString());
 	}
 
-	
+
 	@Test
 	void testMin() {
 
-		String s2 ="-x^2";
-		ComplexFunction cf3 = new ComplexFunction("plus(2x^2,3x^2)");
-
-		function f1 = cf3.initFromString(s2);
-		cf3.min(f1);
-
+		String s ="-x^2";
+		ComplexFunction cf1 = new ComplexFunction("plus(2x^2,3x^2)");
+		function f1 = cf1.initFromString(s);
+		cf1.min(f1);
 		ComplexFunction cf = new ComplexFunction(Operation.Min, new ComplexFunction("plus(2x^2,3x^2"), new Monom("-x^2"));
-		assertTrue(cf3.equals(cf));
-		assertEquals(cf3.toString(), cf.toString());
+		assertTrue(cf1.equals(cf));
+		assertEquals(cf1.toString(), cf.toString());
 	}
+
 
 	@Test
 	void testComp() {
-
-
-		String s2 ="-x^2";
-		ComplexFunction cf3 = new ComplexFunction("plus(2x^2,3x^2)");
-
-		function f1 = cf3.initFromString(s2);
-		cf3.comp(f1);
-
+		String s ="-x^2";
+		ComplexFunction cf1 = new ComplexFunction("plus(2x^2,3x^2)");
+		function f1 = cf1.initFromString(s);
+		cf1.comp(f1);
 		ComplexFunction cf = new ComplexFunction(Operation.Comp, new ComplexFunction("plus(2x^2,3x^2"), new Monom("-x^2"));
-		assertTrue(cf3.equals(cf));
-		assertEquals(cf3.toString(), cf.toString());
-
+		assertTrue(cf1.equals(cf));
+		assertEquals(cf1.toString(), cf.toString());
 	}
 
-	//	@Test
-	//	void testLeft() {
-	//		fail("Not yet implemented");
-	//	}
-	//
-	//	@Test
-	//	void testRight() {
-	//		fail("Not yet implemented");
-	//	}
-	//
-	//	@Test
-	//	void testGetOp() {
-	//		fail("Not yet implemented");
-	//	}
 
+	@Test
+	void testLeft() {
+		ComplexFunction cf1 = new ComplexFunction("plus(2x^2,3x^2)");
+		assertEquals(new Monom("2x^2").toString(), cf1.left().toString());
+	}
+
+	@Test
+	void testRight() {
+		ComplexFunction cf1 = new ComplexFunction("plus(2x^2,3x^2)");
+		assertEquals(new Monom("3x^2").toString(), cf1.right().toString());
+	}
+	
+		@Test
+		void testGetOp() {
+			ComplexFunction cf1 = new ComplexFunction("plus(2x^2,3x^2)");
+			assertEquals(Operation.Plus.toString(), cf1.getOp().toString());
+		}
+		
+		
+		@Test
+		void testEqualsIn() {
+			ComplexFunction cf1 = new ComplexFunction("mul(x,x)");
+			ComplexFunction cf2 = new ComplexFunction("plus(x^2,0)");
+			assertTrue(cf1.equalsIn(cf2, -10, 10, 20));
+			ComplexFunction cf3 = new ComplexFunction("mul(x^2,x)");
+			ComplexFunction cf4 = new ComplexFunction("plus(x^2,0)");
+			assertFalse(cf3.equalsIn(cf4, -10, 10, 20));
+		}
 }
